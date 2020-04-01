f7ae00f3b937f3078f32eeef3aeb4cb4c18eb609
cosam <colmwaikeat@gmail.com>
2020-04-01 18:17:36 +0100

    I have cleaned up the code for the multiprocessing pipeline. First config_dict will require a new input, which is checkmate_output_name and idea behind that is because we can use checkmate for multiple iridis batches, its best to have a name variable to differentiate them. I have also made code that will automatically create the lhe and card checkmate subdirs and then remove them after usage. Also made code to remove non-essetial files generated after batch in calchep batch results dir

--- dm_checker/config_dict.py
+++ dm_checker/config_dict.py
@@ -1,21 +1,30 @@
 config_dict = {
+    #input and output scan paths
     'input_csv_file': '../input_scans/multi_test_in.csv',
     'output_csv_file': '../output_scans/multi_test_out.csv',
+    
+
+    #calchep paths and filenames
     'calchep_dir': '/home/colmsam/MASTERS_project/CalcHEPwork/',
-    'num_events': 1,
-    'fresh_input': True,
     'calchep_batch_file': 'multiprocessing_test_batch_file',
     'calchep_output_events': 'scripting_events',
-    'local': True,
-    'checkmate_dir': '/home/colmsam/checkmate2/bin/',
-    'checkmate_card_file': 'scripting_card.dat',
     
+    #checkmate paths and filnames
+    'checkmate_dir': '/home/colmsam/checkmate2/bin/',
+    'card_file_template': 'SCRIPTING_CHECKMATE_CARD_TEMPLATE.dat',
+    'checkmate_output_name': 'iridis_batch_0',
+    
+    #micromegas paths and filenames
     'micromegas_dir': '/home/colmsam/micromegas/MFDM/',
     'par_file_name': 'data.par',
     'c_file_name': 'the_one_sasha_sent_me',
     'micromegas_output_file': 'micromegas_output_parameters.csv',
     
+    #other stuff
+    'local': True,
+    'num_events': 1,
+    'fresh_input': True,
     'starting_row': 0,
-    'points_in_scan': 2,
+    'points_in_scan': 4,
     'mode': 2
     }
--- dm_checker/multiprocessing_collider_dm_checker.py
+++ dm_checker/multiprocessing_collider_dm_checker.py
@@ -14,7 +14,6 @@ def batch_file_generator(idx, MD1, MDP, MD3, batch_file, calchep_dir, output_eve
     pointless but felt better than returning nothing. Also this way the batch_file name can be passed to next function"""
     path2file = calchep_dir + batch_file
     output_events = output_events_basis + '_' + str(int(idx))
-    print(output_events)
     #asserting mass hierarchy of MFDM
     if MD1<MDP<MD3:
         #read in batch file first
@@ -44,60 +43,32 @@ def batch_file_generator(idx, MD1, MDP, MD3, batch_file, calchep_dir, output_eve
         return None
 
 
-def events_generator(batch_file, calchep_dir, output_events, checkmate_dir):
+def calchep_dir_cleaner(batch_file, calchep_results_dir, output_events):
+    """remove the files we dont need from when we run the batch file"""    
+    run(['rm ' + output_events + '.tgz'], cwd =calchep_results_dir, shell=True)
+    run(['rm ' + output_events + '-single.distr'], cwd =calchep_results_dir, shell=True)
+    return None
+
+
+def events_generator(batch_file, calchep_dir, output_events, checkmate_dir, checkmate_output_name):
     """This takes the batch file editted using the above function, runs it in the calchep_dir,
     creating lhe files called output_events + '-single.lhe.gz', these files are then moved to the checkmate_dir.
     This func returns lhe events name, a tad pointless as its just name of events and not events themselves"""
     #next define what events are gunna be called, as well as the dir where lhe files are held
     calchep_results_dir = calchep_dir + 'batch_results'
     lhe_file = output_events + '-single.lhe'
-    print('\n\n'+lhe_file+'\n\n')
+    lhe_subdir = checkmate_dir + checkmate_output_name + '_lhe_subdir/'
     #first we need to remove the "lock.batch" file (if it exists) in order to do multiple scans in a row
     run(['rm lock.batch'], cwd =calchep_dir, shell=True)
     #next lets run the batch file and generate the lhe events
     run(['./calchep_batch ' + batch_file], cwd=calchep_dir, shell=True)
-    print('\n\nbatch done!!!\n\n')
     #now we unzip the lhe files
     run(['gunzip '+ lhe_file + '.gz'], cwd=calchep_results_dir, shell=True)
     #now we move over the lhe files to the scripting_files folder within checkmate directory
-    run(['cp '+ lhe_file + ' ' + checkmate_dir + 'scripting_lhe_events_folder/.'], cwd=calchep_results_dir, shell=True)
-    #print('done')
+    run(['mv '+ lhe_file + ' ' + lhe_subdir], cwd=calchep_results_dir, shell=True)
+    #lets also clean the calchep dir of the other files we dont need that are generated along with the .lhe files
+    calchep_dir_cleaner(batch_file, calchep_results_dir, output_events)
     return lhe_file
-
-    
-
-def collider_single_point_checker(MD1, MDP, MD3, config_dict):
-    """This function takes a single parameter point and makes a decision if its allowed by bringing the whole pipeline together:
-        1) Creates batch file
-        2) Creates lhe events (using Calchep)
-        3) Makes a decision on those events (using CHECKmate)
-    """
-    #0.5) calchep doesnt like it when the deltas are exactly the same as we get tan(2_theta) = 1/0, so we have to add a slight offset
-    MDP = MDP + 0.0000001
-    MD3 = MD3 + 0.0000002
-    # 1)create the batch file (note variable batch_file is just the str of the name of the batch file)
-    batch_file = batch_file_generator(MD1, MDP, MD3, config_dict['calchep_batch_file'], config_dict['calchep_dir'], config_dict['calchep_output_events'], config_dict['num_events'], config_dict['local'])
-    # 2) Create the lhe files (note variable lhe_file is just the string of the lhe events file name)
-    lhe_file = events_generator(config_dict['calchep_batch_file'], config_dict['calchep_dir'], config_dict['calchep_output_events'], config_dict['checkmate_dir'])
-    # 3) Make a decision on those events, where result is 1 for being allowed, 0 for not allowed
-    result = decision_generator(lhe_file, config_dict['checkmate_dir'], config_dict['checkmate_card_file'])
-    return result
-
-def collider_parameter_space_checker(config_dict):
-    """This function searches multiple parameter points in the parameter space using the collider_single_point_checker function above,
-    it takes the config_dict as the only input. Does not output anything directly but will update the output csv scan and fill it with
-    values of whether each point is allowed or not."""
-    #first lets generate the output csv file
-    generate_output_scan_template_csv(output_csv= config_dict['output_csv_file'], input_csv=config_dict['input_csv_file'], fresh_input=config_dict['fresh_input'], starting_row=config_dict['starting_row'])
-    #now lets loop over the different rows containing the parameter points in our input scan csv
-    for row in tqdm(scan_reader(input_scan_csv= config_dict['input_csv_file']), total=float( config_dict['points_in_scan'] )):
-        #define our masses from our input csv row
-        MD1, MDP, MD3 = float(row[1]), float(row[2]), float(row[3])
-        #for this single parameter point, calculate if its allowed or not. Result = 1 or allowed, 0 if not allowed
-        result = collider_single_point_checker(MD1, MDP, MD3, config_dict)
-        #time to store our result
-        store_result(input_row=row, output_csv=config_dict['output_csv_file'], **result)
-    return None
 
 def all_lhe_events_generator(config_dict):
     print('\n\nGenerating the lhe events using Calchep for all parameter points in scan!!!\n\n')
@@ -110,34 +81,32 @@ def all_lhe_events_generator(config_dict):
         # 1)create the batch file (note variable batch_file is just the str of the name of the batch file)
         output_events, batch_file = batch_file_generator(idx, MD1, MDP, MD3, config_dict['calchep_batch_file'], config_dict['calchep_dir'], config_dict['calchep_output_events'], config_dict['num_events'], config_dict['local'])
         # 2) Create the lhe files (note variable lhe_file is just the string of the lhe events file name)
-        lhe_file = events_generator(config_dict['calchep_batch_file'], config_dict['calchep_dir'], output_events, config_dict['checkmate_dir'])
+        lhe_file = events_generator(config_dict['calchep_batch_file'], config_dict['calchep_dir'], output_events, config_dict['checkmate_dir'], config_dict['checkmate_output_name'])
     print('\n\nDone generating all lhe events for all parameter points in scan!!!\n\n ')
     return None
 
-def decision_generator(idx, row, lhe_file,  checkmate_dir, card_file_template, output_csv_file):
-    """This function takes the lhe events generated from the above function and runs them through
-    checkmate and gets a decision if they're allowed or not. Outputs 1 if allowed, a zero if not allowed"""
-    #lets define some paths our path to our card.dat template and the name of the result file
-    path_2_card_template = checkmate_dir + card_file_template
-    result_name = 'scripting_checkmate_output' + '_' + str(int(idx))
-    #first we need to create the checkmate card, so it refers to right lhe_file name
-    with open(path_2_card_template, 'r') as template:
+def checkmate_card_generator(idx, checkmate_dir, card_file_template, checkmate_output_name, lhe_file, checkmate_result_file_name):
+    """we need to create the checkmate card, so that it refers to right lhe_file, do this by editing a template card file.
+     This outputs the name of the new_card_file which is then needed to run the actual ./Checkmate command"""
+    with open(checkmate_dir + card_file_template, 'r') as template:
         # read a list of lines into data
         changes = template.readlines()
     #make changes to template 
-    changes[3] = 'Name: ' + result_name + '\n'
-    changes[14] = 'Events: ' + 'scripting_lhe_events_folder/' + lhe_file + '\n'
+    changes[3] = 'Name: ' + checkmate_result_file_name + '\n'
+    changes[14] = 'Events: ' + checkmate_output_name+  '_lhe_subdir/' + lhe_file + '\n'
     #and then we write it to a new file!!!!
     new_card_file = 'scripting_card_' + str(int(idx)) + '.dat'
-    with open(checkmate_dir + 'scripting_card_files_folder/' + new_card_file, 'w') as file:
+    with open(checkmate_dir + checkmate_output_name + '_card_subdir/' + new_card_file, 'w') as file:
         file.writelines(changes)
-    #time to run checkmate on our new_card_file which refers to our lhe_file
-    run(['./CheckMATE ' + 'scripting_card_files_folder/' + new_card_file], cwd = checkmate_dir, shell=True)
-    #lets delete the card file and event file to keep things clean
-    #run(['rm ' + 'scripting_card_files_folder/' + new_card_file], cwd = checkmate_dir, shell=True)
-    #run(['rm ' + 'scripting_lhe_events_folder/' + lhe_file], cwd = checkmate_dir, shell=True)
-    #results are held in the following dir, assuming name is unchanged in checkmate card
-    result_dir = checkmate_dir[:-4] + 'results/' + result_name + '/result.txt' #its [:-4] to get move out of bin dir
+    return new_card_file
+
+def clean_checkmate_dir(checkmate_dir, checkmate_output_name, lhe_file, new_card_file):
+    #lets delete the card file and event file to keep things clean and save memory
+    run(['rm ' + checkmate_output_name + '_card_subdir/' + new_card_file], cwd = checkmate_dir, shell=True)
+    run(['rm ' + checkmate_output_name + '_lhe_subdir/' + lhe_file], cwd = checkmate_dir, shell=True)
+    return None
+
+def result_storer(row, result_dir, checkmate_dir, output_csv_file):
     #time to collect and output result from this result_dir
     with open(result_dir, 'r') as file:
         # just way data is formatted, the result is given by below line
@@ -151,36 +120,72 @@ def decision_generator(idx, row, lhe_file,  checkmate_dir, card_file_template, o
         else:
             result = 1
     output = {'r_value': r_value, 'analysis': analysis, 'SR': SR, 'LHC': result}
-    store_result(input_row=row, output_csv=output_csv_file, **output)   
+    store_result(input_row=row, output_csv=output_csv_file, **output)
+    return None   
+
+def decision_generator(idx, row, lhe_file,  checkmate_dir, card_file_template, output_csv_file, checkmate_output_name):
+    """This function takes the lhe events generated from the above function and runs them through
+    checkmate and gets a decision if they're allowed or not. Outputs 1 if allowed, a zero if not allowed"""
+    #lets define some paths our path to our card.dat template and the name of the result file
+    checkmate_result_file_name = checkmate_output_name + '_checkmate_output_' + str(int(idx))
+    #generate our checkmate card file
+    new_card_file = checkmate_card_generator(idx, checkmate_dir, card_file_template, checkmate_output_name, lhe_file, checkmate_result_file_name)
+    #time to run checkmate on our new_card_file which refers to our lhe_file
+    run(['./CheckMATE ' + checkmate_output_name + '_card_subdir/' + new_card_file], cwd = checkmate_dir, shell=True)
+    #clean checkmate dir by removing lhe and card files
+    clean_checkmate_dir(checkmate_dir, checkmate_output_name, lhe_file, new_card_file)
+    #results are held in the following dir, assuming name is unchanged in checkmate card
+    result_dir = checkmate_dir[:-4] + 'results/' + checkmate_result_file_name + '/result.txt' #its [:-4] to get move out of bin dir
+    print(result_dir)
+    #now lets store the result
+    result_storer(row, result_dir, checkmate_dir, output_csv_file)
     return None
+
 
 def multiprocessing_decision_generator(config_dict):
     print('\n\nLets start the parallel processing of the lhe events using checkmate!!!\n\n')
-    pool = mp.Pool(mp.cpu_count())
-    lhe_files = os.listdir(config_dict['checkmate_dir'] + 'scripting_lhe_events_folder/.')
+    #difne our scan_reader, which is a generator we can iterate over to produce each  point in the scan
     input_scan = scan_reader(config_dict['input_csv_file'])
-    checkmate_dir = config_dict['checkmate_dir']
-    card_file_template = config_dict['checkmate_card_file']
-    output_csv_file = config_dict['output_csv_file']
+    #now define the list of lhe files generated from our scan
+    lhe_files = os.listdir(config_dict['checkmate_dir'] + config_dict['checkmate_output_name']+ '_lhe_subdir/')
     #raise error if we dont generate all the lhe files
-    if len(lhe_files)!=config_dict['points_in_scan']:
+    if len(lhe_files)!= config_dict['points_in_scan']:
         raise ValueError('Number of lhe files produced does not match number of points in scan!!!')
     #the actual parallel processing...
+    pool = mp.Pool(mp.cpu_count())
     for idx, lhe_file in tqdm(enumerate(lhe_files), total=float( config_dict['points_in_scan'])):
         row = next(input_scan)
-        pool.apply_async(decision_generator, args=(idx, row, lhe_file,  checkmate_dir, card_file_template, output_csv_file) )
+        pool.apply_async(decision_generator, args=(idx, row, lhe_file,  config_dict['checkmate_dir'], config_dict['card_file_template'], config_dict['output_csv_file'], config_dict['checkmate_output_name']) )
     pool.close()
     pool.join()
     print('\n\nFINISHED!!!\n\n')
     return None
 
+def generate_checkmate_subdirs(config_dict):
+    #first generate the lhe_subdir
+    run(['mkdir ' + config_dict['checkmate_output_name'] + '_lhe_subdir/'], cwd = config_dict['checkmate_dir'], shell=True)
+    #then generate the card_subdir
+    run(['mkdir ' + config_dict['checkmate_output_name'] + '_card_subdir/'], cwd = config_dict['checkmate_dir'], shell=True)
+    return None
+
+def remove_checkmate_subdirs(config_dict):
+    #first remove the lhe_subdir
+    run(['rm -rf ' + config_dict['checkmate_output_name'] + '_lhe_subdir/'], cwd = config_dict['checkmate_dir'], shell=True)
+    #then remove the card_subdir
+    run(['rm -rf ' + config_dict['checkmate_output_name'] + '_card_subdir/'], cwd = config_dict['checkmate_dir'], shell=True)
+    return None
+
 def complete_multiprocessing_pipeline(config_dict):
     #first lets generate the output csv file
     generate_output_scan_template_csv(output_csv= config_dict['output_csv_file'], input_csv=config_dict['input_csv_file'], fresh_input=config_dict['fresh_input'], starting_row=config_dict['starting_row'])
+    #second lets generate the lhe_subdir and card_subdir, that we will need to store our checkmate related files
+    generate_checkmate_subdirs(config_dict)
     #now lets generate the lhe files for all the points in the scan
     all_lhe_events_generator(config_dict)
     #lets start the parallel processing of the lhe events using checkmate!!!
     multiprocessing_decision_generator(config_dict)
+    #once were done, we can remove the checkmate subdirs
+    remove_checkmate_subdirs(config_dict)
     return None
 
 if __name__ == '__main__':
